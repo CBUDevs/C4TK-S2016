faeb165ba18f9c309bfbcd3ec72d78313580dc16
Caleb Solorio <c.solorio@att.net>
2016-03-05 05:56:10 +0000

    improved login modal

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-$(document).ready(function () {
 
+var loginSwitch = function(num) {
+  if (num > 0) {
+    $(".login-content").show();
+    $(".register-content").hide();
+  } else {
+    $(".register-content").show();
+    $(".login-content").hide();
+  }
+}
+
+$(document).ready(function () {
+  $(".register-content").hide(); // hides the registration content on the login modal.
   $('.modal-trigger').leanModal();
-  console.log(root);
+  
 });
