3003c9f9b7ed0e2e2fc5ec8c02e41700ef733a5a
Caleb Solorio <c.solorio@att.net>
2016-03-05 06:43:49 +0000

    Added register functions

--- app.js
+++ app.js
@@ -1,25 +1,4 @@
 
-<<<<<<< HEAD
-    $('.modal-trigger').leanModal();
-    console.log(root);
-});
-
-var login = function () {
-    var uname = document.getElementById("email").value;
-    var pass = document.getElementById("password").value;
-    loginGet(uname, pass);
-};
-
-var register(){
-    
-};
-
-var changeHeading(){
-    
-};
-
-var
-
 var loginSwitch = function(num) {
   if (num > 0) {
     $(".login-content").show();
@@ -28,6 +7,30 @@ var loginSwitch = function(num) {
     $(".register-content").show();
     $(".login-content").hide();
   }
+}
+
+var registerUser = function() {
+  var email = $("#user-email").val();
+  var password = $("#user-password").val();
+  var username = $("#user-username").val();
+  var denomination = $("#user-denomination").val();
+  var zipcode = $("#user-zipcode");
+  var bio = $("#bio").val();
+  
+  registerUserPost(bio, denomination, email, password, "", username);
+}
+
+var registerChurch = function() {
+  var email = $("#church-email").val();
+  var password = $("#church-password").val();
+  var username = $("#church-username").val();
+  var denomination = $("#church-denomination").val();
+  var link = $("#church-link").val();
+  var zipcode = $("#church-zipcode");
+  var description = $("#bio").val();
+
+  registerChurchPost(denomination, description, email, link, password, "", username, zipcode);
+  
 }
 
 $(document).ready(function () {
